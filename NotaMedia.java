280bcd1f0e48d2ee3fc3751d90e60f93c76fa4d5
profesorjacinto15 <profesor.jacinto15@gmail.com>
2022-03-31 23:44:53 +0100

    Commit inicial

---
+++ NotaMedia.java
@@ -0,0 +1,29 @@
+package NotaMedia;
+
+import java.util.Scanner;
+
+public class NotaMedia {
+	
+	public static void main (String args[]) {
+
+		Scanner teclado = new Scanner(System.in);
+		int cantidadNotas, numero;
+		float total, nota;
+        
+	    System.out.print("Indique cuantas notas desea introducir: ");
+	    cantidadNotas = teclado.nextInt();
+	        
+	    total = 0.0f;
+	    numero = 1;
+	    do {
+	        System.out.print("Introduzca la nota " + numero + ": ");
+	        nota = teclado.nextFloat();
+	        total = total + nota;
+		    numero = numero + 1;
+	    } while (numero <= cantidadNotas);
+	        
+	    float notaMedia = total/cantidadNotas;
+	    System.out.println("La nota media es: " + notaMedia);    
+	}
+	
+}
